c80b24784b1bdc98d56f5d5090c2e5defe6a21a1
BHISHMA <bhishmadandekar@gmail.com>
2026-04-30 17:00:13 +0530

    app.py fixed

--- Web_Application/backend/app.py
+++ Web_Application/backend/app.py
@@ -130,6 +130,4 @@ def predict():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    import os
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=5000, debug=True)
